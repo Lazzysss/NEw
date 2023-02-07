498d7c4a113f89323eba9174135e883cb449d2c4
Lazzysss <arturhalknazarov6@gmail.com>
2023-02-07 15:34:27 +0500

    FIrst

---
+++ src/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+from quotes.views import Register
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('register/success/',
+        TemplateView.as_view(template_name='registration/success.html'), 
+        name='register-success'),
+    path('register/', Register.as_view(), name='register'),
+    path('admin/', admin.site.urls),
+    path('quote/', include('quotes.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('pages.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
